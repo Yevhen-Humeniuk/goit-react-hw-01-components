883002551bb75429f3d4a3b75c24e724c9b17e76
Yevhen-Humeniuk <gumenyukevgen2@gmail.com>
2022-09-10 14:45:58 +0300

    add-json-files-for-tasks

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,14 @@
+<ul class="friend-list">
+  <!-- Довільна кіл-сть FriendListItem -->
+</ul>
+
+
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>
+
+import friends from "path/to/friends.json";
+
+<FriendList friends={friends} />;
